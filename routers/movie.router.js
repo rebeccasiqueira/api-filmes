936c61342089507a295a9b03684b9e675ce87736
Junior <83678470+OzielUnidesc@users.noreply.github.com>
2022-12-11 22:09:33 -0300

    Delete routers directory

--- routers/movie.router.js
+++
@@ -1,9 +0,0 @@
-const movieController = require('../controllers/AuthControllerM');
-
-router.get('/', movieController.getAll);
-router.post('/', movieController.create);
-router.get('/:movieId', movieController.getById);
-router.put('/:movieId', movieController.updateById);
-router.delete('/:movieId', movieController.deleteById);
-
-module.exports = router;
