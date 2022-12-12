33de6b24011934681249b505ffd12d779800d209
Junior <83678470+OzielUnidesc@users.noreply.github.com>
2022-12-11 22:11:00 -0300

    Add files via upload

--- src/models/Movies.js
+++ src/models/Movies.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+var generateId = require('./plugins/generateId.js');
 
 const MovieSchema = new Schema({
+ id: {
+      type: Number, 
+      required: true, 
+      index: {
+      unique: true
+      }
+ }, 
  name: {
     type: String,
      trim: true,  
@@ -11,7 +19,12 @@ const MovieSchema = new Schema({
     type: Date,
     trim: true,
     required: true
- }
+ }, 
+actors: [{
+  type: mongoose.Schema.ObjectId, 
+  ref: 'Actor'
+}]
 });
 
+MovieSchema.plugin(generateId());
 module.exports = mongoose.model('Movie', MovieSchema)
